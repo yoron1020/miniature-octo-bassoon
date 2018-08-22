44ced71bf1ac44df9d9d07e889b724ba99ed952e
Ban <fzd9887@gmail.com>
2018-08-22 20:27:26 +0800

    Add more comment and delete wrong function

--- problem_set_volume_1/101/101.py
+++ problem_set_volume_1/101/101.py
@@ -9,31 +9,43 @@ pile a over b:把a和a上面的所有block放到b上
 總共2個動作:歸位x上block 把x和x上所有block放到x'上(如果x上面沒有別的block就會做出"把a放到b上")
 """
 
+#宣告一個空的一維陣列
 blockList=[]
 
-def _initialBlock(blockNum):
-	for i in range(len(blockList[blockNum])):
-		#putBackNum=blockList[blockNum][-1]
-		blockList[blockList[blockNum][-1]].append(blockList[blockNum][-1])
-		del blockList[blockNum][-1]
+
+#在一維陣列的索引裡面再加上一個一維陣列就可以製作出一個二維陣列
+def _makeList(blockNum):
+	for i in range(blockNum):	#假設blockNum是10，就要做出最大10x10的陣列，因為還加上值擴充，所以目前是10x1的二維陣列
+		blockList.append([])	#在每個一維陣列的索引再加上一格一維陣列
+		blockList[i].append(i)	#[[0],[1],[2],[3],[4],[5],[6],[7],[8],[9]]
+
+#歸位
+def _initialBlock(blockNum):  
+	for i in range(len(blockList[blockNum])):	#假設blockNum=5，迴圈的長度就是二維陣列裡第5個索引的一維陣列長度，ex:[[0],[1],[2],[3],[4],[5,6,7,8,9]]，長度是5
+		#putBackNum=blockList[blockNum][-1]		#第5個一維陣列的最後一個
+		blockList[blockList[blockNum][-1]].append(blockList[blockNum][-1])	#把第5個一維陣列的最後一個值加到該值代表的一維陣列的最後面
+		del blockList[blockNum][-1]	#因為已經把值加到該去的地方了，所以把第5個索引的值移除
 
 
+#有錯的function XDDDD
+"""
 def _pileBlock(blockNum1, blockNum2):
 	blockList[blockNum2].extend(blockList[blockNum1])
 	del blockList[blockNum1][-1] #delete last element in two dimension list
 
 def _moveBlock(blockNum1, blockNum2):
 	blockList[blockNum2].extend(blockList[blockNum1])
+"""
 
-def _makeList(blockNum):
-	for i in range(blockNum):
-		blockList.append([]) #making two dimension array by append [] to the list
-		blockList[i].append(i)
-	#return blockList
+#移動block
+#要先找到blockNum1和blockNum2在二維陣列的位置，
+def moveBlock(blockNum1, blockNum2):
+	
 
+
+#輸入block數量，然後產生一個二維陣列
 num=int(input())
 _makeList(num)
-#print(blockList)
 
 while True:
 	command=input()
